2a15fc9a98638e38650c18a31f80904deec215e4
Jhr29j <juniorjoestudent@gmail.com>
2025-05-24 00:43:26 -0400

    Subo todos los archivos del proyecto POA

--- Includes/registrar_usuario.php
+++ Includes/registrar_usuario.php
@@ -2,9 +2,8 @@
 // Iniciar sesión al principio
 session_start();
 
-// Rutas correctas para los includes
-require_once __DIR__.'config.php';
-require_once __DIR__.'db.php';
+require_once __DIR__.'/config.php';
+require_once __DIR__.'/db.php';
 
 // Verificar si la solicitud es POST
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
